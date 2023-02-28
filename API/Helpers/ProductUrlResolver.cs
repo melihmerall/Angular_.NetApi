235dcac0138728b9b521b9ff2085ed57e339b5a0
Melih <melih16.meral@gmail.com>
2023-03-01 01:27:06 +0300

    UrlResolver Added

---
+++ API/Helpers/ProductUrlResolver.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Dtos;
+using AutoMapper;
+using Core.Entities;
+
+namespace API.Helpers
+{
+    public class ProductUrlResolver : IValueResolver<Product, ProductToReturnDto, string>
+    {
+        private readonly IConfiguration _cfg;
+        public ProductUrlResolver(IConfiguration cfg)
+        {
+            _cfg = cfg;
+        }
+
+        public string Resolve(Product source, ProductToReturnDto destination, string destMember, ResolutionContext context)
+        {
+            if(!string.IsNullOrEmpty(source.PictureUrl))
+            {
+                return _cfg["ApiUrl"]+ source.PictureUrl;
+            }
+            return null;
+        }
+    }
+}
